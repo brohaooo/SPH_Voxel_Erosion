3a9c1e0089efcee105be5159feeb62370502a3e7
Lin Xinhao <brohaooo@gmail.com>
2023-10-25 23:28:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -120,7 +120,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "SPH_Voxel_Erosion", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
